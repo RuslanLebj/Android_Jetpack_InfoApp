c05a768e9527470a4cd5731f8e5bfa70799f63c7
dressgecko <dressgecko@gmail.com>
2023-12-19 20:01:59 +0500

    Added top bar and drawer menu

--- app/src/main/java/com/example/infoapp/MainActivity.kt
+++ app/src/main/java/com/example/infoapp/MainActivity.kt
@@ -6,17 +6,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.infoapp.ui.theme.InfoAppTheme
+import com.example.infoapp.ui_components.DrawerMenu
+import com.example.infoapp.ui_components.MainTopBar
 
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            val topBarTitle = remember {
+                mutableStateOf("")
+            }
+            Scaffold(
+                scaffoldState = scaffoldState,
+                topBar = {
+                    MainTopBar(
+                        title = topBarTitle.value,
+                        scaffoldState
+                    )
+                },
+                drawerContent = {
+                    DrawerMenu()
+                }
+            ) {
+            }
         }
     }
 }
